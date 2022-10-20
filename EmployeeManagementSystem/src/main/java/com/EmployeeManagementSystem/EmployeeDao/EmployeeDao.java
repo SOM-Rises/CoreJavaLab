cf77c653783050b9279a8598fc1b76e3a8572930
Somnath Hazra <somnath6376hazra@gmail.com>
2022-10-20 17:10:06 +0530

    20Oct Lab Question Commit

---
+++ EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/EmployeeDao/EmployeeDao.java
@@ -0,0 +1,15 @@
+package com.EmployeeManagementSystem.EmployeeDao;
+
+import com.EmployeeManagementSystem.model.Employee;
+
+public interface EmployeeDao {
+
+	void add();
+
+	boolean delete(int id);
+
+	Employee find(int id);
+
+	void display();
+
+}
